9bba2bfe60a0a63c8b148bf6906aacc28b61787b
Michael Oboho <michael.oboho@gmail.com>
2025-02-21 04:51:05 +0100

    Start version

---
+++ script.js
@@ -0,0 +1,29 @@
+// ANIMATION FOR SIGN IN/UP PAGE
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// HAMBURGER MENU FOR MOBILE
+function toggleMenu() {
+	const menu = document.querySelectorAll('.menu-links, .harmburger-icon');
+	
+	menu.forEach(element =>{
+	  element.classList.toggle("open");
+	})
+  }
+
+  document.addEventListener("click", function (event) {
+    const menu = document.querySelector(".menu-links");
+
+    if (!menu.contains(event.target)) {
+        menu.classList.toggle("open");
+    }
+});
