920cc5ffad6b7610ef63ad4e754927d06b7f5fc5
Phantom Coder <130623659+MgsSlayer@users.noreply.github.com>
2025-06-09 23:59:12 +0100

    Update product_detail.jsljbgyknv

--- product_detail.js
+++ product_detail.js
@@ -7,14 +7,15 @@ const itemID = params.get("id");
 fetch(`https://magbeads-backend.onrender.com/products/${itemID}`)
   .then((res) => res.json())
   .then((item) => {
-    const image = document.getElementById("image_container");
+    const image = document.getElementById("pimage");
+    const imageCon = document.getElementById("image_container");
     const name = document.getElementById("product_name");
     const price = document.getElementById("product_id");
     const details = document.getElementById("product_desc_page");
 
-    image.style.background = `url('${item.image}')`;
-    image.style.backgroundSize = "cover";
-    image.style.backgroundPosition = "center";
+    image.src = `${item.image}`;
+    imageCon.style.backgroundSize = "cover";
+    imageCon.style.backgroundPosition = "center";
     name.innerHTML = `${item.name}`;
     price.innerHTML = `${item.price}`;
     details.innerHTML = `${item.description}`;
