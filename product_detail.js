b314fa25c851bca5a9c6ff73709ab9a4a7b61fa7
Michael <michael.oboho@gmail.com>
2025-06-09 16:31:51 +0100

    Minor changes7

--- product_detail.js
+++ product_detail.js
@@ -1,29 +1,25 @@
 console.log(window.location.href);
 console.log(window.location.search);
 //FETCH PRODUCT AND DISPLAY DETAILS
- const params = new URLSearchParams(window.location.search);
- const itemID = params.get('id');
-
+const params = new URLSearchParams(window.location.search);
+const itemID = params.get("id");
 
 fetch(`https://magbeads-backend.onrender.com/products/${itemID}`)
-.then((res) => res.json())
-.then((item) => {
+  .then((res) => res.json())
+  .then((item) => {
+    const image = document.getElementById("image_container");
+    const name = document.getElementById("product_name");
+    const price = document.getElementById("product_price");
+    const details = document.getElementById("product_desc");
 
-
-const image = document.getElementById('image_container');
-const name  = document.getElementById('product_name');
-const price = document.getElementById('product_id');
-const details = document.getElementById('product_desc_page');
-
-image.style.background = `url('${item.image}')`;
-image.style.backgroundSize = 'cover';
-image.style.backgroundPosition = 'center';
-name.innerHTML = `${item.name}`;
-price.innerHTML = `${item.price}`;
-details.innerHTML = `${item.description}`
-console.log(item);
-
-})
-.catch((err) => {
-  console.log(`${err.name}`);
-})
+    image.style.background = `url('${item.image}')`;
+    image.style.backgroundSize = "cover";
+    image.style.backgroundPosition = "center";
+    name.innerHTML = `${item.name}`;
+    price.innerHTML = `${item.price}`;
+    details.innerHTML = `${item.description}`;
+    console.log(item);
+  })
+  .catch((err) => {
+    console.log(`${err.name}`);
+  });
